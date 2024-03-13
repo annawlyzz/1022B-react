5040be15499a6930f1fcf2b940bc39d3e3075bd2
annawlyzz <ana.pego@estudante.ifms.edu.br>
2024-03-13 10:15:06 -0400

    Meu primeiro componente.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-//Componente React
 import './App.css'
+import MeuComponente from './componentes/MeuComponente'
 function App() {
-  let nome = "Anna <3"
-  let sobrenome = "Eleutério"
-  return (
-    <div>
-      <h1 className="nome">Nome: {nome}</h1>
-      <p className="sobrenome">{sobrenome}</p>
-    </div>
-   
-  )//Retorna JSX - JavaScript XML
+
+    let nome = "Ana Clara"
+    let sobrenome = "Eleutério"
+    return (
+      <div>
+        <MeuComponente/>
+     <h1 className="nome">Nome: {nome}</h1>    
+     <p className="sobrenome">{sobrenome}</p>    
+      </div>
+
+  )// Retorna JSX - JavaScript XML
 }
 
 export default App
